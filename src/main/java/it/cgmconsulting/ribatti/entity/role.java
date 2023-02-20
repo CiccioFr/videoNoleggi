44336f2b5e24b2cfba49aa0c9305024ddca258ea
CiccioFr <cicciofr.net@tiscali.it>
2023-02-20 12:03:35 +0100

    Entity: Role

--- src/main/java/it/cgmconsulting/ribatti/entity/role.java
+++ src/main/java/it/cgmconsulting/ribatti/entity/role.java
@@ -1,2 +1,18 @@
-package it.cgmconsulting.ribatti.entity;public class role {
+package it.cgmconsulting.ribatti.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity @Getter @Setter @NoArgsConstructor
+public class role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long roleId;
+
+    @Column(length = 20, unique = true, nullable = false)
+    private String roleName;
 }
