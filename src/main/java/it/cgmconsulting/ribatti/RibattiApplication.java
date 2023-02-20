fa7abb62854c13a1f4b182b2cbb206eb7250613c
CiccioFr <cicciofr.net@tiscali.it>
2023-02-20 09:38:52 +0100

    Creazione Prog VideoNoleggi

---
+++ src/main/java/it/cgmconsulting/ribatti/RibattiApplication.java
@@ -0,0 +1,13 @@
+package it.cgmconsulting.ribatti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibattiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibattiApplication.class, args);
+	}
+
+}
